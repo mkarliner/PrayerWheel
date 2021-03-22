e48e1dff31bbde919ba75c4817a4146f01c10fe1
Michael Karliner <mike@BigMac.karliner.org>
2021-03-22 17:27:26 +0000

    NEW: Publish prayer on button press

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from machine import Pin
 import network
+import usocket
+import usys
 import time
 from umqtt.simple import MQTTClient
 from machine import Pin
 
+
+#IMPORTANT request clientID, username and password from info@prayerwheel.world!!!
 
 def do_connect():
     import network
@@ -17,31 +21,44 @@ def do_connect():
     print('network config:', sta_if.ifconfig())
     
 
+    
+
 def sub_cb(topic, msg):
+    global relay
     print((topic, msg))
+    relay.value(1)
+    time.sleep(10)
+    relay.value(0)
 
 def main( server="mqtt.prayerwheel.world",):
-    c = MQTTClient("umqtt_client", server)
-    print("MQTT connected")
+    clientID = "xxxxx"
+    print('MQTT start')
+    c = MQTTClient(clientID, usocket.getaddrinfo(server, 1883)[0][4][0], user="yyyy", password="zzzzz")
     c.set_callback(sub_cb)
     c.connect()
     c.subscribe(b"prayers/out")
-    relay = Pin(5, Pin.OUT)
+    time.sleep(1)
+    print("MQTT connected")
+    print("Boo")
     while True:
-        # Blocking wait for message
-        print(c.wait_msg())
-        relay.value(1)
-        time.sleep(10)
-        relay.value(0)
+        c.check_msg()
+        if(button.value() == 0):
+            print('Publishing prayer')
+            c.publish(b'prayers/in', clientID)
+            time.sleep(1)
+        time.sleep_ms(50)
 
-
+c = 'asf'
+relay = Pin(5, Pin.OUT)
+button = Pin(4, Pin.IN, Pin.PULL_UP)
 
 if __name__ == "__main__":
-    try:
-        do_connect() 
-        main()
-        print("Main exited, restarting")
-    except OSError as e:
-        print("Connection failure")
-        time.sleep(5)
-        machine.reset()
+    while True:
+        try:
+            do_connect() 
+            main()
+            print("Main exited, restarting")
+        except OSError as e:
+            usys.print_exception(e)
+            time.sleep(5)
+#         machine.reset()
